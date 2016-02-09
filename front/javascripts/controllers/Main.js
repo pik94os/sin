469794b43ad5f4145d16225823778bf75bed3e5b
Roman Shulgin <pik94os.po@gmail.com>
2016-02-09 15:49:56 +0300

    repeat fot one message block

--- front/javascripts/controllers/Main.js
+++ front/javascripts/controllers/Main.js
@@ -8,12 +8,24 @@ define(['./module','jquery', 'bootstrap'],function(controllers,$){
         $scope.$on('html100', function(event,html100){
             $scope.html100 = html100;
         });
-        $scope.friends = '{}';
+        $scope.friends = [];
         $http.get('/ajax/friends.json').then(function (response) {
             if(response.data.err){
 
             }else{
                 $scope.friends = response.data.friends;
+            }
+        }, function (response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+        $scope.user = {};
+        $http.get('/ajax/user.json').then(function (response) {
+            if(response.data.err){
+
+            }else{
+                $scope.user = response.data.user;
             }
         }, function (response) {
             // called asynchronously if an error occurs
--- front/javascripts/controllers/Message.js
+++ front/javascripts/controllers/Message.js
@@ -1,6 +1,6 @@
 define(['./module','jquery', 'slimScroll'],function(controllers,$){
     'use strict';
-    controllers.controller('Message',['$rootScope','$scope', function($rootScope,$scope){
+    controllers.controller('Message',['$rootScope','$scope','$http', function($rootScope,$scope,$http){
         $('title').text('Сообщения');
         $rootScope.$on('$viewContentLoaded',function(event, viewConfig){
             $('#dialog-list > div').slimScroll({
@@ -9,6 +9,17 @@ define(['./module','jquery', 'slimScroll'],function(controllers,$){
                 wheelStep: '5',
                 distance: '-5px'
             });
+        });
+        $scope.messages = [];
+        $http.get('/ajax/messages.json').then(function (response) {
+            if(response.data.err){
+
+            }else{
+                $scope.messages = response.data.messages;
+            }
+        }, function (response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
         });
         $rootScope.$broadcast('html100', true);
     }])
