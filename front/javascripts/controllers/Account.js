f55fa494195c4a082967e842216c8368432fa968
pik <pik94os.po@gmail.com>
2016-02-11 18:31:07 +0300

    users data

--- front/javascripts/controllers/Account.js
+++ front/javascripts/controllers/Account.js
@@ -6,5 +6,16 @@ define(['./module','jquery', 'bootstrap'],function(controllers,$){
     controllers.controller('Account',['$rootScope','$scope', function($rootScope,$scope){
         $rootScope.$broadcast('html100', false);
         $('title').text('Главная');
+
+        $rootScope.$on('$viewContentLoaded',function(event, viewConfig){
+            $('#wrap_bot_message > div').slimScroll({
+                height: '100%',
+                color: '#99c3d9',
+                wheelStep: '5',
+            });
+        });
+        $scope.editStatus = function(){
+
+        }
     }])
 });
--- front/javascripts/controllers/Main.js
+++ front/javascripts/controllers/Main.js
@@ -1,7 +1,7 @@
 /**
  * Created by pik on 06.01.16.
  */
-define(['./module','jquery', 'bootstrap'],function(controllers,$){
+define(['./module','jquery', 'slimScroll', 'bootstrap'],function(controllers,$){
     'use strict';
     controllers.controller('Main',['$scope','$http', function($scope,$http){
         $scope.html100=false;
@@ -44,6 +44,27 @@ define(['./module','jquery', 'bootstrap'],function(controllers,$){
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
             });
+        });
+
+        function wrap_soc(){
+            $('.wrap_soc').height($('.bg-ava-big').height()+'px');
+        }
+
+        $(window).resize( function(){
+            wrap_soc();
+        });
+
+
+        $(document).on('click', '#status', function(){
+            $(this).css('width', '96%');
+            $(this).children('span').html('');
+            $(this).children('input').removeClass('hide').focus();
+        }).on('click', '#btn_bot', function(){
+            wrap_soc();
+        }).on('blur', '#status input', function(){
+            $('#status').css('width', '180px');
+            $('#status span').html('Изменить статус');
+            $('#status input').addClass('hide');
         });
     }])
 });
