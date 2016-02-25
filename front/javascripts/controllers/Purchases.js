57abfffc0781f26dea4fecf1e4ecad515d744c68
Roman Shulgin <pik94os.po@gmail.com>
2016-02-25 12:52:50 +0300

    Покупки

--- front/javascripts/controllers/Purchases.js
+++ front/javascripts/controllers/Purchases.js
@@ -3,8 +3,19 @@
  */
 define(['./module','jquery'],function(controllers,$){
     'use strict';
-    controllers.controller('Purchases',['$rootScope','$scope', function($rootScope,$scope){
+    controllers.controller('Purchases',['$rootScope','$scope','$http', function($rootScope,$scope,$http){
         $('title').text('Покупки');
         $rootScope.$broadcast('html100', false);
+        $scope.categories =[];
+        $http.get('/ajax/categ.json').then(function (response) {
+            if(response.data.err){
+
+            }else{
+                $scope.categories = response.data.categories;
+            }
+        }, function (response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
     }])
 });
