3aed73322cbf47432371ef63740711f66f6ce7cc
4ant0m <de@de.com>
2016-04-25 15:11:31 +0300

    pg driver, db

--- server/lib/ormdb.js
+++ server/lib/ormdb.js
@@ -3,16 +3,22 @@ var orm = require("orm");
 orm.connect("pg://postgres:iliveforever@localhost/postgres", function (err, db) {
   if (err) throw err;
 
-    var Person = db.define("user", {
-        id        : {type: 'serial', key: true},
-        name      : String,
-        surname   : String,
-        age       : Number, 
-        male      : Boolean,
-        city      : String, 
-        photo     : String,
-        language  : ['ru'],
-        data      : Object 
+    var User = db.define("user", {
+        id               : {type: 'serial', key: true},
+        name             : String,
+        surname          : String,
+        age              : Number, 
+        male             : Boolean,
+        maritalstatus    : Boolean,
+        city             : String, 
+        photo            : String,
+        language         : ['ru'],
+        online           : Boolean,
+        statustxt        : String,
+        background       : String,
+        withbackground   : Boolean,
+        recordwall       : Boolean,
+        data             : Object 
     }, {
         methods: {
             fullName: function () {
@@ -23,29 +29,32 @@ orm.connect("pg://postgres:iliveforever@localhost/postgres", function (err, db) 
             age: orm.enforce.ranges.number(18, undefined, "under-age")
         }
     });
+    User.hasMany('friends', User, {}, {key: true});
+    User.hasMany('grandparents', User, {}, {reverse: 'grandchildren', key: true});
+    User.hasMany('parents', User, {}, {reverse: 'children'});
+    User.hasMany('brothers', User, {}, {key: true});
 
     // add the table to the database
     db.sync(function(err) { 
         if (err) throw err;
 
         // add a row to the person table
-        Person.create({ id: 1, name: "John", surname: "Doe", age: 27 }, function(err) {
-            if (err) throw err;
+       // User.create({ id: 1, name: "John", surname: "Doe", age: 27 }, function(err) {
+            //if (err) throw err;
 
                 // query the person table by surname
-                Person.find({ surname: "Doe" }, function (err, people) {
+                User.find({ surname: "Doe" }, function (err, people) {
                     // SQL: "SELECT * FROM person WHERE surname = 'Doe'"
                     if (err) throw err;
 
                     console.log("People found: %d", people.length);
-                    console.log("First person: %s, age %d", people[0].fullName(), people[0].age);
+                    //console.log("First person: %s, age %d", people, people);
+                    for(var i in people)
+                        console.log(people[i]);
 
-                    people[0].age = 16;
-                    people[0].save(function (err) {
-                        // err.msg = "under-age";
                 });
             });
 
-        });
+        //});
     });
 });
